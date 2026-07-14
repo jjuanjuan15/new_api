042bf7a502279377ff0d86107d473988c627acce
chetoe <j.juanjuan15@gmail.com>
2026-07-14 18:46:00 +0200

    add ia

--- app/main.py
+++ app/main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI, HTTPException, Query, Depends, Header
 import os
 from services.service import funcion_elisa,funcion_putos
+from huggingface_hub import InferenceClient
+
+app = FastAPI()
+
+client = InferenceClient(
+    provider="hf-inference",
+    api_key="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"
+)
 app = FastAPI()
 
 PASSWORD = os.getenv("PSW", "putogordo12")
@@ -26,3 +34,16 @@ async def elisa():
 @app.get("/puto", dependencies=[Depends(comprobar_password)])
 async def putos():
     return await funcion_putos()
+@app.get("/ia")
+async def ia(prompt: str):
+    respuesta = client.chat.completions.create(
+        model="HuggingFaceTB/SmolLM2-135M-Instruct",
+        messages=[
+            {"role": "user", "content": prompt}
+        ],
+        max_tokens=100
+    )
+
+    return {
+        "respuesta": respuesta.choices[0].message.content
+    }
